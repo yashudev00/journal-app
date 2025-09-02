b250e2334802a06a0b0b5810301b66a871674360
yashudev00 <vdyashu09@gmail.com>
2025-09-02 17:48:35 +0530

    added Role based authentication

--- journalApp/src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
@@ -22,6 +22,7 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .antMatchers("/journal/**","/user/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
--- journalApp/src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -45,4 +45,10 @@ public class UserService {
     public User findByUserName(String userName){
         return userRepository.findByUserName(userName);
     }
+
+    public void saveAdmin(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER","ADMIN"));
+        userRepository.save(user);
+    }
 }
